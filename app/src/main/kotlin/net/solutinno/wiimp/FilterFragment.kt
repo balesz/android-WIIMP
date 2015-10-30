7ee051d9bb6e77999b643ca138d45493390fb396
Bálint Böde <balint.bode@gmail.com>
2015-10-30 14:34:14 +0100

    Initial commit

---
+++ app/src/main/kotlin/net/solutinno/wiimp/FilterFragment.kt
@@ -0,0 +1,13 @@
+package net.solutinno.wiimp
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class FilterFragment : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater?.inflate(R.layout.filter, container)
+    }
+}
---
+++ app/src/main/kotlin/net/solutinno/wiimp/MainActivity.kt
@@ -0,0 +1,43 @@
+package net.solutinno.wiimp
+
+import android.os.Bundle
+import android.support.design.widget.NavigationView
+import android.support.v7.app.ActionBarDrawerToggle
+import android.support.v7.app.AppCompatActivity
+import android.view.Gravity
+import android.view.View
+import kotlinx.android.synthetic.main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.main)
+
+        setSupportActionBar(main_toolbar)
+
+        val toggle = ActionBarDrawerToggle(this, main_drawer, main_toolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close)
+        main_drawer.setDrawerListener(toggle)
+        toggle.syncState()
+
+        main_fab.setOnClickListener(onFabClick)
+
+        main_navigation.setNavigationItemSelectedListener(onNavigationItemSelected)
+    }
+
+    override fun onBackPressed() {
+        if (main_drawer.isDrawerOpen(Gravity.START))
+            main_drawer.closeDrawer(Gravity.START)
+        else super.onBackPressed()
+    }
+
+    val onNavigationItemSelected = NavigationView.OnNavigationItemSelectedListener {
+        true
+    }
+
+    val onFabClick = View.OnClickListener {
+        main_drawer.openDrawer(Gravity.END)
+    }
+}
