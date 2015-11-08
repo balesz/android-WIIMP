d4ab93cd20e34893e8303ca761c764bd76da928b
Bálint Böde <balint.bode@gmail.com>
2015-11-08 23:05:58 +0100

    Implementing FilterItemView

--- app/src/main/kotlin/net/solutinno/wiimp/widgets/FilterItemView.kt
+++ app/src/main/kotlin/net/solutinno/wiimp/widgets/FilterItemView.kt
@@ -4,82 +4,90 @@ import android.content.Context
 import android.content.DialogInterface
 import android.support.v7.app.AlertDialog
 import android.util.AttributeSet
-import android.widget.Button
 import android.widget.FrameLayout
-import android.widget.TextView
-import android.widget.Toast
-import kotlinx.android.synthetic.filter_item.view.button
-import kotlinx.android.synthetic.filter_item.view.subtitle
-import kotlinx.android.synthetic.filter_item.view.title
+import com.appeaser.sublimepickerlibrary.datepicker.DatePickerFunctions
+import com.appeaser.sublimepickerlibrary.datepicker.SublimeDatePicker
+import kotlinx.android.synthetic.filter_item.view.button1
+import kotlinx.android.synthetic.filter_item.view.text1
+import kotlinx.android.synthetic.filter_item.view.text2
 import net.solutinno.wiimp.R
-import net.solutinno.wiimp.widgets.FilterItemView.Type.values
+import org.joda.time.DateTime
 import java.util.*
 
 class FilterItemView : FrameLayout {
 
-    enum class Type (val value: Int) {
-        SPINNER(0), DATE_RANGE(1), LIST(2), MULTI_SELECT_LIST(3);
-        companion object {
-            fun valueOf (value: Int) : Type? = values.firstOrNull { it.value == value }
-        }
+    companion object {
+        val TYPE_LIST = 0
+        val TYPE_DATE_RANGE = 1
+        val TYPE_MULTI_SELECT_LIST = 2
     }
 
-    val titleText: String?
-    val subtitleText: String?
-    val type: Type?
+    val type: Int
+
     val entries: Array<CharSequence>?
-    val selected: ArrayList<String> = arrayListOf()
+
+    val selectedValues: ArrayList<CharSequence> = arrayListOf()
+
+    var title: CharSequence?
+        get() = text1?.text
+        set(value) { text1?.text = value }
+
+    var subtitle: CharSequence?
+        get() = text2?.text
+        set(value) { text2?.text = value }
 
     constructor(ctx: Context, attrs: AttributeSet? = null) : super(ctx, attrs) {
+        addView(inflate(context, R.layout.filter_item, null).apply {
+            layoutParams = generateDefaultLayoutParams()
+        })
         val array = ctx.theme.obtainStyledAttributes(attrs, R.styleable.FilterItemView, 0, 0)
-        titleText = array.getString(R.styleable.FilterItemView_title)
-        subtitleText = array.getString(R.styleable.FilterItemView_subtitle)
-        type = Type.valueOf(array.getInt(R.styleable.FilterItemView_type, -1))
+        title = array.getString(R.styleable.FilterItemView_title)
+        subtitle = array.getString(R.styleable.FilterItemView_subtitle)
+        type = array.getInt(R.styleable.FilterItemView_type, -1)
         entries = array.getTextArray(R.styleable.FilterItemView_entries)
         array.recycle()
         setupView()
     }
 
     private fun setupView () {
-        addView(inflate(context, R.layout.filter_item, null).apply {
-            layoutParams = generateDefaultLayoutParams()
-            title.setupTitle()
-            subtitle.setupSubtitle()
-            button.setupButton()
-        })
-    }
-
-    private fun TextView.setupTitle() {
-        text = titleText
-    }
-
-    private fun TextView.setupSubtitle() {
-        text = subtitleText ?: entries?.first() ?: "Subtitle"
-    }
-
-    private fun Button.setupButton() {
-        if (entries != null && arrayOf(Type.LIST, Type.SPINNER).containsRaw(type)) {
-            setOnClickListener {
-                AlertDialog.Builder(context)
-                        .setItems(entries, DialogInterface.OnClickListener { dialogInterface, i ->
-                            this@FilterItemView.subtitle.text = entries[i]
-                        })
-                        .show()
-            }
-        } else if (entries != null && type == Type.MULTI_SELECT_LIST) {
-            setOnClickListener {
-                AlertDialog.Builder(context)
-                        .setMultiChoiceItems(entries, null, DialogInterface.OnMultiChoiceClickListener { dialogInterface, i, b ->
-                            if (b) selected.add(entries[i].toString())
-                            else selected.remove(entries[i].toString())
-                            this@FilterItemView.subtitle.text = selected.joinToString()
-                        })
-                        .show()
-            }
-        } else {
-            setOnClickListener {
-                Toast.makeText(it.context, "$entries", Toast.LENGTH_SHORT).show()
+        if (entries != null && type == TYPE_LIST) {
+            subtitle = entries.first()
+        }
+        button1.setOnClickListener {
+            AlertDialog.Builder(context).apply {
+                when (type) {
+                    TYPE_LIST -> listClick()
+                    TYPE_DATE_RANGE -> dateRangeClick()
+                    TYPE_MULTI_SELECT_LIST -> multiSelectListClick()
+                }
             }
         }
+    }
+
+    private fun AlertDialog.Builder.dateRangeClick() {
+        setView(SublimeDatePicker(context).apply {
+            init(2015, 11, 8, DatePickerFunctions.OnDateChangedListener { sdp, y, m, d ->
+                subtitle = DateTime(y, m+1, d, 12, 0).toString("yyyy-MM-dd")
+            })
+        }, 72, 16, 0, 16)
+        show()
+    }
+
+    private fun AlertDialog.Builder.listClick() {
+        if (entries == null) return
+        setItems(entries, DialogInterface.OnClickListener { dialogInterface, i ->
+            subtitle = entries[i]
+        })
+        show()
+    }
+
+    private fun AlertDialog.Builder.multiSelectListClick() {
+        if (entries == null) return
+        setMultiChoiceItems(entries, null, DialogInterface.OnMultiChoiceClickListener { di, i, b ->
+            if (b) selectedValues.add(entries[i])
+            else selectedValues.remove(entries[i])
+            subtitle = selectedValues.joinToString()
+        })
+        show()
     }
 }
